b06a0e9fa74df408f478740e40c271b470d98d57
Winsteen04 <106991076+Winsteen04@users.noreply.github.com>
2025-01-31 23:10:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Enable CORS
+app.use(cors());
+
 app.get("/api/info", (req, res) => {
     const response = {
-        email: "winstonubong@gmail.com"
-      "current_datetime" :"2025-01-31T09: 30;00Z
-     "github_url: "https://github.com/Winsteen04/hng_task_0"
+        email: "winstonubong@gmail.com",
+        current_datetime: new Date().toISOString(), 
+        github_url: "https://github.com/Winsteen04/hng_task_0"
     };
 
     res.json(response);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
